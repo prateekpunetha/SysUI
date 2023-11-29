b2982a65045183db2e5f2cbebf20a6aa06faeb64
prateekpunetha <hi@prateekpunetha.dev>
2023-11-29 08:25:59 +0530

    sysui: create installation script
    Signed-off-by: prateekpunetha <hi@prateekpunetha.dev>

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo apt update
+
+sudo apt install python3 python3-pip \
+	python-is-python3 python3-tk zenity -y
+
+echo "Installing requirements..."
+pip3 install -r requirements.txt
+echo done!
